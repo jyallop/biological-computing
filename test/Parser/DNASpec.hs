52beb5c24f743ed8edd07d3f8f1e88635f7e7f82
Jonathon <jyallop21@gmail.com>
2021-12-04 21:07:19 -0700

    updates test to reflect maybe changes

--- test/Parser/DNASpec.hs
+++ test/Parser/DNASpec.hs
@@ -8,16 +8,22 @@ spec :: Spec
 spec = do
   describe "DNA Parser Test" $ do
     it "Parse empty string" $
-      parseDNA "" `shouldBe` []
+      parseDNA "" `shouldBe` Just []
 
+    it "invalid input returns nothing" $
+      parseDNA "ASCXVASDF" `shouldBe` Nothing
+      
     it "Parse Single A" $
-      parseDNA "A" `shouldBe` [A]
+      parseDNA "A" `shouldBe` Just [A]
 
     it "Parse Single C" $
-      parseDNA "C" `shouldBe` [C]
+      parseDNA "C" `shouldBe` Just [C]
 
     it "Parse Single G" $
-      parseDNA "G" `shouldBe` [G]
+      parseDNA "G" `shouldBe` Just [G]
 
     it "Parse Single T" $
-      parseDNA "T" `shouldBe` [T]
+      parseDNA "T" `shouldBe` Just [T]
+
+    it "Parse Sample Sequence" $
+      parseDNA "ACGACTGTCAGTA" `shouldBe` Just [A, C, G, A, C, T, G, T, C, A, G, T, A]
