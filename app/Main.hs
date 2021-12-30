0ee7b339041b49cceac3542f694441a401724447
Jonathon <jyallop21@gmail.com>
2021-12-30 11:44:58 -0700

    adds RNA translation and cleans up input and output

--- app/Main.hs
+++ app/Main.hs
@@ -1,27 +1,45 @@
 module Main (main) where
 
 import Data.DNA
+import Data.RNA
 import Parser.DNA
 import System.Console.ArgParser
 import Data.Text(strip, pack, unpack)
 
-data Computation = Count Bool String
+data Computation =
+    Count Bool String
+  | Translate Bool String
   deriving (Eq, Show)
 
 computationParser :: IO (CmdLnInterface Computation)
 computationParser = mkSubParser
   [
     ("count", setAppDescr (mkDefaultApp countParser "count")
-                          "Counts nucleotides in a sequence given by a file or a command line argument")
+                          "Counts nucleotides in a sequence given by a file or a command line argument"),
+    ("translate", setAppDescr (mkDefaultApp translateParser "translate")
+                              "translates a DNA sequence into an RNA sequence")
   ]
 
 countParser :: ParserSpec Computation
 countParser = Count `parsedBy` boolFlag "file" `Descr` "when set will read the input string as a file name"
                     `andBy` reqPos "input string" `Descr` "The string containing the DNA sequence or a filename"
 
+translateParser :: ParserSpec Computation
+translateParser = Translate `parsedBy` boolFlag "file" `Descr` "when set will read the input string from a file"
+                            `andBy` reqPos "input string" `Descr` "the string containing the DNA sequence or the filename"
+
 main :: IO ()
 main = computationParser >>= (flip runApp) compute
 
 compute :: Computation -> IO ()
-compute (Count True file) = readFile file >>= (putStrLn . show . fmap count . parseDNA . unpack . strip . pack) 
-compute (Count False string) = (putStrLn . show . fmap count . parseDNA) string
+compute (Count True file) = readFile file >>= (printOutput . fmap count . parseDNA . stripInput) 
+compute (Count False string) = (printOutput . fmap count . parseDNA) string
+compute (Translate True file) = readFile file >>= (printOutput . fmap translate . parseDNA . stripInput) 
+compute (Translate False string) = (printOutput . fmap (foldr (\n s -> (show n) ++ s) "") . fmap translate . parseDNA) string
+
+printOutput :: (Show a) => Either String a -> IO ()
+printOutput (Left error) = putStrLn error
+printOutput (Right output) = putStrLn $ show output
+
+stripInput :: String -> String
+stripInput = unpack . strip . pack
